a7194ab1acda4d5fa906038ce3b62d3d0d2021bc
LouGit <LouGit@users.noreply.github.com>
2026-08-14 16:09:45 +0000

    deploy: b560d34f40c09e55445b65577fe62be420b6348c

--- cargo/ko/searcher-c2a407aa.js
+++ cargo/ko/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-db2c62e5.js',
+                path_to_root + 'searchindex-b11eb613.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
